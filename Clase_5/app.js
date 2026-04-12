49a534d297ad7d6e18c5c27de2b9ffbd4f73409e
Andrew <andresdavid.dev.1405@gmail.com>
2026-04-12 12:35:46 -0500

    estudiando

---
+++ Clase_5/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express"; // require -> commonJSimport { randomUUID } from "node:crypto";
+import {corsMiddleware }  from "./middlewares/cors.js";
+import { moviesRouter } from "./routes/movies.js";
+
+const app = express();
+app.use(json());
+app.disable("x-powered-by"); // deshabilitar el header X-Powered-By: Express
+app.use(corsMiddleware())
+app.use("/movies", moviesRouter);
+
+const PORT = process.env.PORT ?? 1234;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
